43e2447cf621ae60eae2ff1ea2eac7d50e1bd4b7
carlos.s <carlos.s.satoritech@gmail.com>
2022-11-02 09:53:18 -0600

    first commit

---
+++ pruebaPractica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pruebaPractica
+//
+//  Created by Satoritech 31 on 02/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
